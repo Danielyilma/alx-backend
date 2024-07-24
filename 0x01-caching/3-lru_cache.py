f87959610a7e87b8fd62a49ce193c22a86c1ecc9
Danielyilma <yilmad752@gmail.com>
2024-07-24 15:51:06 +0300

    LRU Cache

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -20,19 +20,34 @@ class LRUCache(BaseCaching):
         '''
         if len(self.cache_data) >= self.MAX_ITEMS and \
                 key not in self.cache_data.keys():
-            num = list(self.counts.keys).sort()[0]
+            num = sorted(list(self.counts.keys()))[0]
             print(f'DISCARD: {self.counts[num]}')
             del self.cache_data[self.counts[num]]
+            del self.counts[num]
+        elif key in self.cache_data.keys():
+            for i, value in self.counts.items():
+                if value == key:
+                    num = i
+                    break
             del self.counts[num]
 
         if not key or not item:
             return
 
+        self.count += 1
+        self.counts[self.count] = key
         self.cache_data[key] = item
 
     def get(self, key):
         '''get data from cache data attribute by the key'''
+        num = -1
         if key in self.cache_data.keys():
+            for i, value in self.counts.items():
+                if value == key:
+                    num = i
+                    break
+            del self.counts[num]
             self.count += 1
             self.counts[self.count] = key
+        print(self.counts, key)
         return self.cache_data.get(key, None)

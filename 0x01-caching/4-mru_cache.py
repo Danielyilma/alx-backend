6a9b2e92d8d048597a1ae25a0de175b27f782749
Danielyilma <yilmad752@gmail.com>
2024-07-24 15:59:02 +0300

    MRU Cache

---
+++ 0x01-caching/4-mru_cache.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+'''implements MRUCache'''
+from base_caching import BaseCaching
+
+
+class MRUCache(BaseCaching):
+    '''
+        MRUCache defines:
+            defines a constant for the order of insertion
+            put and get method for the cache
+    '''
+    def __init__(self):
+        self.counts = {}
+        self.count = -1
+        super().__init__()
+
+    def put(self, key, item):
+        '''
+            puts data to the cache with MRU replacement algorithm
+        '''
+        if len(self.cache_data) >= self.MAX_ITEMS and \
+                key not in self.cache_data.keys():
+            num = sorted(list(self.counts.keys()), reverse=True)[0]
+            print(f'DISCARD: {self.counts[num]}')
+            del self.cache_data[self.counts[num]]
+            del self.counts[num]
+        elif key in self.cache_data.keys():
+            for i, value in self.counts.items():
+                if value == key:
+                    num = i
+                    break
+            del self.counts[num]
+
+        if not key or not item:
+            return
+
+        self.count += 1
+        self.counts[self.count] = key
+        self.cache_data[key] = item
+
+    def get(self, key):
+        '''get data from cache data attribute by the key'''
+        num = -1
+        if key in self.cache_data.keys():
+            for i, value in self.counts.items():
+                if value == key:
+                    num = i
+                    break
+            del self.counts[num]
+            self.count += 1
+            self.counts[self.count] = key
+        return self.cache_data.get(key, None)

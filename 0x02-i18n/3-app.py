22fbb65c571d8d779d3e9b4539ba91ec5808064b
Danielyilma <yilmad752@gmail.com>
2024-07-30 22:29:57 +0300

    babel translation

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+'''adding a babel configration'''
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+class Config:
+    '''Babel config class'''
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+def get_locale():
+    '''function to be invoked for each request'''
+    return request.accept_languages.best_match(
+        app.config["LANGUAGES"]
+    )
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app, locale_selector=get_locale)
+
+
+@app.route("/")
+def index():
+    '''index page'''
+    return render_template("1-index.html")
+
+
+if __name__ == "__main__":
+    app.run(host="localhost", port=5000, debug=True)

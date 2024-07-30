1e49d3593a8ef0c2b1c13a82b6b6479b0e1f61e6
Danielyilma <yilmad752@gmail.com>
2024-07-30 21:08:14 +0300

    flask setup

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -5,6 +5,7 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
     '''index page'''
@@ -12,4 +13,4 @@ def index():
 
 
 if __name__ == "__main__":
-    app.run(host="localhost", port=5000 ,debug=True)
+    app.run(host="localhost", port=5000, debug=True)
